01bf97ad56e410311bc07d7ca3e8de68854f3efe
jshgod <1379981194@qq.com>
2020-04-15 22:48:28 +0800

    debug ray pos

--- python_vers/racecar_controller.py
+++ python_vers/racecar_controller.py
@@ -4,7 +4,7 @@ from racecar import Racecar
 
 class RacecarController(Racecar):
 
-  def __init__(self, bullet_client, urdfRootPath='', start_pos = [0, 0, .2], start_ori = [0, 0, 0]):
+  def __init__(self, bullet_client, urdfRootPath, time_step, start_pos = [0, 0, .2], start_ori = [0, 0, 0]):
     assert isinstance(start_pos, (list, tuple)), \
       "Type Error: start pos should be a list or typle..."
     assert len(start_pos) == 3, \
@@ -18,7 +18,11 @@ class RacecarController(Racecar):
     super().__init__(bullet_client, urdfRootPath, start_pos, start_ori)
     
     self._pos = start_pos
-    self._ori = start_ori
+    self._vel = 0
+    self._ori = self._p.getQuaternionFromEuler(start_ori)
+    self._yaw_rate = 0
+    self._last_yaw = 0
+    self._time_step = time_step
 
     self._maxForceUpperbound = 100
     self._speedMultiplierUpperbound = 100
@@ -26,7 +30,7 @@ class RacecarController(Racecar):
 
     self._maxForce = 20
     self._speedMultiplier = 20.
-    self._steeringMultiplier = 0.5
+    self._steeringMultiplier = 1.0
 
     # don't change them
     self._steering_links = [0, 2]
@@ -41,11 +45,19 @@ class RacecarController(Racecar):
 
   @property
   def orient(self):
-    return self._orient
+    return self._p.getEulerFromQuaternion(self._ori)
+
+  @property
+  def yaw_rate(self):
+    return self._yaw_rate
 
   @property
   def pos(self):
     return self._pos
+  
+  @property
+  def vel(self):
+    return self._vel
 
   @property
   def maxForce(self):
@@ -80,8 +92,8 @@ class RacecarController(Racecar):
   def stepSim(self, drawRays = False, drawStep = 5):
     assert isinstance(drawRays, bool), \
       "drawRays should be boolen type"
-    self._pos, self._orient = self._p.getBasePositionAndOrientation(self._carId)
-    self._rays.rayPos = [self._pos[0], self._pos[1], 0.3]
+    self._pos, self._ori = self._p.getBasePositionAndOrientation(self._carId)
+    self._rays.set_sensor_pos([self._pos[0], self._pos[1], 0.3])
     hit_pos = self._rays.scan_env()
     # don't support multi-robot until now
     if drawRays:
@@ -89,14 +101,15 @@ class RacecarController(Racecar):
     return hit_pos
         
   def apply_action(self, motorCommands):
-      targetVelocity = motorCommands[0] * self._speedMultiplier
+      self._vel = motorCommands[0] * self._speedMultiplier
       steeringAngle = motorCommands[1] * self._steeringMultiplier
+      self._yaw_rate, self._last_yaw = (steeringAngle - self._last_yaw) / self._time_step, steeringAngle
 
       for motor in self._motorized_wheels:
         self._p.setJointMotorControl2(self._carId,
                                       motor,
                                       self._p.VELOCITY_CONTROL,
-                                      targetVelocity=targetVelocity,
+                                      targetVelocity=self._vel,
                                       force=self._maxForce)
       for steer in self._steering_links:
         self._p.setJointMotorControl2(self._carId,
@@ -109,17 +122,25 @@ if __name__ == '__main__':
   import pybullet as pb
   import pybullet_data
   from bullet_client import BulletClient
-
+  import time
+  time_step = 1./600
   p = BulletClient(pb.GUI)
   p.setAdditionalSearchPath(pybullet_data.getDataPath())
   planID = p.loadURDF('plane.urdf')
   p.setGravity(0,0,-10)
-  car = RacecarController(p, pybullet_data.getDataPath())
-  car.apply_action([0.7, 0.5])
-  i = 0
+  car = RacecarController(p, pybullet_data.getDataPath(), time_step)
+  vel = 0.7
+  angle = 1.0
+  i = 0.7
   while True:
     i += 1
-    car.stepSim(drawStep = 5)
+    car.apply_action([vel, angle])
+    if i > 200:
+      angle = -1.0
+    if i > 400:
+      vel = 0
+    pb.stepSimulation()
+    car.stepSim(True,drawStep = 5)
     print("actionDimension: ", car.maxForce)
     print("pos: ", car.pos)
     print("orient: ", car.orient)
@@ -132,4 +153,5 @@ if __name__ == '__main__':
     # car.speedMultiplier = -50
     car.steeringMultiplier = 1.
     # car.steeringMultiplier = 3.
+    time.sleep(time_step)
   p.disconnect()
