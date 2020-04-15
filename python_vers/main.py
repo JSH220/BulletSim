f84573aa78c8c38ab579b7409ee1057cfca7d266
IamWangYunKai <615349118@163.com>
2020-04-15 23:48:27 +0800

    modify ray coordinate

--- python_vers/main.py
+++ python_vers/main.py
@@ -18,7 +18,7 @@ additional_path = pbd.getDataPath()
 timeStep = 1./60.
 space = 2
 offsetY = space
-matrix = [1,1]
+matrix = [3,2]
 
 def setObstacles(number):
     for i in range(number):
@@ -53,29 +53,43 @@ if __name__ == "__main__":
         for i in range(matrix[0]):
             offset=[offsetX,offsetY,0.5]
             #robot = client.loadURDF('racecar/racecar.urdf', offset)
-            robot = RacecarController(client, additional_path, offset, [0, 0, 0])
+            robot = RacecarController(client, additional_path, timeStep, offset, [0, 0, 0])
             robots.append(robot)
             offsetX += space 
-        offsetY += space 
+        offsetY += space
     
     client.configureDebugVisualizer(client.COV_ENABLE_RENDERING,1)
-
-    for i in range (200):
+    
+    #position = [robots[0].pos[0], robots[0].pos[1]+2, robots[0].pos[2]]
+    #obstacle_id = pb.createCollisionShape(pb.GEOM_BOX,halfExtents=[0.4, 0.4, 0.5])
+    #pb.createMultiBody(baseMass=9999,baseCollisionShapeIndex=obstacle_id, basePosition=position)
+    for i in range (2000):
         for robot in robots:
             if type(robot) == type(0):
                 break
-            robot.apply_action([1.0, 0.5])
+            robot.apply_action([1.5, 0.5])
             hit_pos = robot.stepSim(False, 200)
 
             if i > 50:
                 #print(len(hit_pos))
+                yaw = robot.orient[2]
+
                 hit_pos = np.array(hit_pos)
-                _x = [val - robot.pos[0] for val in hit_pos[:,0]]
-                _y = [val - robot.pos[1] for val in hit_pos[:,1]]
-                x.extend(_x)
-                y.extend(_y)
+                _x = np.array([val - robot.pos[0] for val in hit_pos[:,0]])
+                _y = np.array([val - robot.pos[1] for val in hit_pos[:,1]])
+
+                x_ = _x*np.cos(yaw) - _y*np.sin(yaw)
+                y_ = _x*np.sin(yaw) + _y*np.cos(yaw)
+                x.extend(list(x_))
+                y.extend(list(y_))
+
         client.stepSimulation()
         #sleep(0.01)
     print('Finish !')
+    #x.extend(list(x_))
+    #y.extend(list(y_))
+    plt.figure(num=1, figsize=(5, 5))
+    plt.xlim((-3, 3))
+    plt.ylim((-3, 3))
     plt.scatter(x, y, s=1, alpha=0.6)
     plt.show()
